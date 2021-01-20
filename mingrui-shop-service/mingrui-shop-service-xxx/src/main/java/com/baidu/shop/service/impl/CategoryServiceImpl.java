f05107c3abae4e32eab59a231c617acc7ba272e5
Seven <aaa>
2021-01-20 19:34:17 +0800

    .

--- mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -6,28 +6,20 @@ import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * @ClassName CategoryServiceImpl
  * @Description: TODO
  * @Author lss
- * @Date 2021/1/18
+ * @Date 2021/1/20
  * @Version V1.0
  **/
-@RestController
-public class CategoryServiceImpl extends BaseApiService implements CategoryService {
-    @Autowired
+public class CategoryServiceImpl extends BaseApiService implements CategoryService{
+    @Resource
     private CategoryMapper categoryMapper;
 
-    @Override
-    public Result<List<CategoryEntity>> getCategoryByPid(Integer pid) {
-        CategoryEntity categoryEntity = new CategoryEntity();
-        categoryEntity.setParentId(pid);
 
-        List<CategoryEntity> list = categoryMapper.select(categoryEntity);
-        return this.setResultSuccess(list);
-    }
 }
